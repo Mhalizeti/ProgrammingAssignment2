c6a920ed81b016db769ee49140fa1652ee6eb8a5
Mhalizeti <mhalizeti@gmail.com>
2016-09-10 20:38:36 -0500

    Assignment 2 week 3 ready
    I added a pair of functions that cache the inverse of a matrix. This is
    especially important for large matrices calculations with high
    computation times.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These pair of functions cache the inverse of a matrix. This is especially
+## important for large matrices calculations with high computation times.
 
-## Write a short comment describing this function
+## This first function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## set the values of the matrix and initialize the values of the inverse matrix
+  mat_inverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    mat_inverse <<- NULL
+  }
+  
+  ## get the values of the matrix
+  get <- function() x
+  
+  ## set the inverse matrix
+  setinverse <- function(solve) mat_inverse <<- solve
+  
+  ## get the inverse matrix
+  getinverse <- function() mat_inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This second function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return the inverse matrix 'mat_inv' from the cache if it has been
+  ## previously calculated
+  mat_inv <- x$getinverse()
+  if(!is.null(mat_inv)) {
+    message("getting inverse matrix from cache")
+    return(mat_inv)
+  }
+  
+  ## Calculate and return 'mat_inv', wchich is the inverse matrix of 'x' 
+  data <- x$get()
+  mat_inv <- solve(data, ...)
+  x$setinverse(mat_inv)
+  mat_inv
+  
 }
